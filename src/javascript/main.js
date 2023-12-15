0d756fc563dadc654a119f582e65342e3f55f859
Christian buaka <Christian.buaka@hotmail.com>
2023-12-15 17:27:47 +0100

    Added sass, axios and started with some javascript

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '/src/sass/main.css'
+import axios from 'axios';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const apiUrl = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy';
 
-setupCounter(document.querySelector('#counter'))
+const fetchQuestions = async () => {
+  try {
+    const response = await axios.get(apiUrl);
+   response.data.results;
+    console.log(response.data.results);
+  } catch (error) {
+    console.error('Error fetching: ', error);
+  }
+};
+
+fetchQuestions();
+
