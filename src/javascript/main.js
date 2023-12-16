d43817864df3691be90a966bc8796cc5f6db8dc9
Christian buaka <Christian.buaka@hotmail.com>
2023-12-16 18:41:23 +0100

    Created a displayQuestion function

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,15 +3,37 @@ import axios from 'axios';
 
 const apiUrl = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=easy';
 
+let currentQuestionIndex = 0;
+let questions = [];
+
 const fetchQuestions = async () => {
   try {
     const response = await axios.get(apiUrl);
-   response.data.results;
+   questions = response.data.results;
     console.log(response.data.results);
   } catch (error) {
     console.error('Error fetching: ', error);
   }
 };
 
-fetchQuestions();
+// fetchQuestions();
+
+const displayQuestion = () => {
+
+  const questionContainer = document.getElementById('question');
+  const optionsContainer = document.getElementById('options');
+  const currentQuestion = questions[currentQuestionIndex];
+
+  questionContainer.innerHTML = currentQuestion.questions;
+  optionsContainer.innerHTML = '';
+
+  currentQuestion.incorrect_answers.forEach((option, index)=>{
+  const optionElement = document.createElement('div');
+  optionElement.innerHTML = `<input type="radio" name="option" value="${index}"> ${option}`;
+  optionsContainer.appendChild(optionElement);
+    });
+  const correctOptionElement = document.createElement('div');
+  correctOptionElement.innerHTML = `<input type="radio" name="option" value="correct">${currentQuestion.correct_answer}`;
+  optionsContainer.appendChild(correctOptionElement);
+}
 
